14eb34436c792d3c183d16119a7fde08786cf523
samspieth <sbspieth@uw.edu>
2019-03-05 12:22:32 -0800

    view interactive demo

--- exercise3.py
+++ exercise3.py
@@ -2,7 +2,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+import uszipcode
+from uszipcode import SearchEngine
+search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info database
+zipcode = search.by_zipcode("98105")
+print(zipcode)
 # Exercise 3
 # Create a visualization with interactivity. Similar to Exercise 2,
 # you can decide what you want to use as data and
--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 # Note: import the app of what you are working on
 #from initial_demo import app
 #from exercise1 import app
-from exercise2 import app
+#from exercise2 import app
 # from exercise2 import app
+from vis_dash_interaction_connected_graphs import app
 
 server = app.server
 
